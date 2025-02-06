498f85ba59559245a1efbe7c985b76ce26e59966
NathanLeRoux29 <nathanlerouxpro@gmail.com>
2025-02-06 18:18:22 +0100

    Ajout Account view et Connexion View Client Leger

---
+++ Kuby_ClientLeger/src/components/card/Adresse.vue
@@ -0,0 +1,45 @@
+<template>
+    <h2 class="text-md ms-15 mt-5">
+    Mon adresse
+    </h2>
+    <v-card
+    elevation="5"
+    max-width="40%"
+    rounded="lg"
+    class="mx-auto my-8"
+    >
+    <v-card-text>
+    <v-row>
+    <v-col>
+    <div class="py-1">
+    <h2 class="text-md ">
+    Stéphane Agréable
+    </h2>
+    </div>
+    <div class="py-1">
+    <h3 class="text-md font-weight-light">
+    4 rue du Bonheur de la sarthe
+    </h3>
+    </div>
+    <div class="py-1">
+    <h3 class="text-md font-weight-light">
+    123456
+    </h3>
+    </div>
+    <div class="py-1">
+    <h3 class="text-md font-weight-light">
+    JoieVille
+    </h3>
+    </div>
+    <div class="py-1">
+    <h3 class="text-md font-weight-light">
+    AmusementCountry
+    </h3>
+    </div>
+    </v-col>
+    </v-row>
+    </v-card-text>
+    </v-card>
+    </template>
+<script setup lang="ts">
+</script>
--- Kuby_ClientLeger/src/views/Account.vue
+++ Kuby_ClientLeger/src/views/Account.vue
@@ -1,12 +1,16 @@
-<template>  
+
+<template>
     <v-container>
+    <TitleComponent :title="title"/>
+    <Order />
+    <Adresse />
     </v-container>
-  </template>
-  
-  <script setup lang="ts">
-  
-  import { ref } from 'vue';
-  
-  
-  
-  </script>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import TitleComponent from '@/components/title/TitleComponent.vue'
+    import Order from '@/components/card/Order.vue';
+    import Adresse from '@/components/card/Adresse.vue';
+    const title = ref("Votre compte");
+</script>
